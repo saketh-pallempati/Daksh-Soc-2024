66352d7c6beecb9c191408ca33b823134e574e59
DenserMeerkat <mragulmanoharan@gmail.com>
2024-03-06 19:43:16 +0530

    feat: three models

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ForgotPassword from "./Components/ForgotPassword";
 import ResetPassword from "./Components/ResetPassword";
 import Dashboard from "./Components/Dashboard";
+import Dashboard1 from "./Components/Dashboard1";
 import Adminpage from "./Components/AdminPage";
 import Vaults from "./Components/Vaults";
 import LandingPage from "./Components/LandingPage";
@@ -13,6 +14,7 @@ function App() {
         <Route path="/forgotPassword" element={<ForgotPassword />}></Route>
         <Route path="/resetPassword/:token" element={<ResetPassword />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/dashboard1" element={<Dashboard1 />}></Route>
         <Route path="/admin.php" element={<Adminpage />}></Route>
         <Route path="/vaults" element={<Vaults />}></Route>
       </Routes>
